31ed094d7d6e489bbe7b0b2aecc5f5e6102cece5
bigliuliu <1552000425@qq.com>
2024-03-20 15:50:21 +0800

    实现防抖和节流函数

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      It's deployed by vercel successfully When you see this website.
-    </p>
+    <button @click="fd">测试防抖</button>
+    <p>防抖：每次触发都会重新计算定时器，最终只执行一次</p>
+    <button @click="jl">测试节流</button>
+    <p>节流：间隔固定时间执行一次</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+import { ref ,defineComponent} from "vue";
+
+export default defineComponent ({
+  setup() {
+    // 防抖函數
+    let ftimer = null;
+    const fd = () => {
+      clearTimeout(ftimer);
+      ftimer = setTimeout(() => {
+        console.log("FD success !");
+      }, 1000);
+    };
+    //节流函数
+    let jtimer = ref(null);
+    let canRun = true;
+    const jl = () => {
+      if (!canRun) {
+        return;
+      }
+      canRun = false;
+      jtimer.value = setTimeout(() => {
+       console.log("JL success !")
+        canRun = true;
+      }, 1000);
+    };
+    return {
+      fd,
+      jl,
+    };
+  },
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
